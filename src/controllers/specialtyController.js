1ff90c2c791ac2d070fda38ad431861c45c8a9ba
hermitcrab <hermitcrab@pop-os.localdomain>
2022-06-19 11:02:41 +0700

    add new route

--- src/controllers/specialtyController.js
+++ src/controllers/specialtyController.js
@@ -26,7 +26,23 @@ let getAllSpecialty = async (req, res) => {
     }
 }
 
+let getDetailSpecialtyById = async (req, res) => {
+    try {
+        let response = await specialtyService.getDetailSpecialtyById(req.query.id, req.query.location);
+        return res.status(200).json(response);
+    } catch (error) {
+        console.error("createSpecialty: ", error);
+        return res.status(200).json({
+            errCode: -1,
+            message: 'Error from server'
+        })
+    }
+}
+
+
+
 module.exports = {
     createSpecialty: createSpecialty,
     getAllSpecialty: getAllSpecialty,
+    getDetailSpecialtyById: getDetailSpecialtyById,
 }
--- src/services/specialtyService.js
+++ src/services/specialtyService.js
@@ -1,6 +1,6 @@
 import db from '../models/index'
 
-let createSpecialty = async (data) => {
+let createSpecialty = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
             if (!data.name || !data.image || !data.descriptionHTML || !data.descriptionMarkdown) {
@@ -28,7 +28,7 @@ let createSpecialty = async (data) => {
     })
 }
 
-let getAllSpecialty = async (data) => {
+let getAllSpecialty = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
             let data = await db.Specialty.findAll()
@@ -36,7 +36,7 @@ let getAllSpecialty = async (data) => {
                 data.map(item => {
                     item.image = new Buffer(item.image, 'base64').toString('binary')
                     return item
-               })
+                })
             }
             resolve({
                 errCode: 0,
@@ -49,7 +49,58 @@ let getAllSpecialty = async (data) => {
     })
 }
 
+let getDetailSpecialtyById = (inputId, location) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+
+            if (!inputId || !location) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameters',
+                })
+            } else {
+                let data = {}
+                data = await db.Specialty.findOne({
+                    where: { id: inputId },
+                    attributes: ['descriptionHTML', 'descriptionMarkdown']
+                })
+                if (data) {
+                    let doctorSpecialty = []
+                    if (location === 'ALL') {
+                        doctorSpecialty = await db.Doctor_Infor.findAll({
+                            where: {
+                                specialtyId: inputId,
+                          
+                            },
+                            attributes: ['doctorId', 'provinceId']
+                        })
+                    } else {
+                        doctorSpecialty = await db.Doctor_Infor.findAll({
+                            where: {
+                                specialtyId: inputId,
+                                provinceId: location
+                            },
+                            attributes: ['doctorId', 'provinceId']
+                        })
+                    }
+                    data.doctorSpecialty = doctorSpecialty;
+                } else data = {}
+
+                resolve({
+                    errCode: 0,
+                    errMessage: 'OK',
+                    data
+                })
+
+            }
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
 module.exports = {
     createSpecialty: createSpecialty,
     getAllSpecialty: getAllSpecialty,
+    getDetailSpecialtyById: getDetailSpecialtyById,
 }
